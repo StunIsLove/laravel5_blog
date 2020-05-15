165a4548bb91f75b554947d01f2fe6ab99c4bf75
StunIsLove <ksmgames@yandex.ru>
2020-05-15 18:02:46 +0600

    Add method for edit categories

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -27,11 +27,11 @@ class CategoryController extends Controller
      */
     public function create()
     {
-       return view('admin.categories.create', [
+        return view('admin.categories.create', [
           'category'   => [],
           'categories' => Category::with('children')->where('parent_id', '0')->get(),
           'delimiter'  => ''
-       ]);
+        ]);
     }
 
     /**
@@ -64,7 +64,11 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-        //
+        return view('admin.categories.edit', [
+          'category'   => $category,
+          'categories' => Category::with('children')->where('parent_id', '0')->get(),
+          'delimiter'  => ''
+        ]);
     }
 
     /**
